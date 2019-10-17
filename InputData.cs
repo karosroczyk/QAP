da2c318b1c2ea3964e54ee37e4f81395de6d237d
karosroczyk <50304244+karosroczyk@users.noreply.github.com>
2019-10-17 10:06:36 +0200

    Dodaj pliki projektów.

---
+++ InputData.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QAP
+{
+    class InputData
+    {
+        public List<List<int>> MatrixD { get; } //Read only
+        public List<List<int>> MatrixF { get; } //Read only
+
+        public InputData()
+        {
+            /*MatrixD = new List<List<int>>(){
+                new List<int>(){0,1,3,4,1,3},
+                new List<int>(){1,0,1,5,1,1},
+                new List<int>(){3,1,0,2,1,1},
+                new List<int>(){4,5,2,0,1,1},
+                new List<int>(){1,1,1,1,0,2},
+                new List<int>(){3,2,3,1,2,0}
+            };
+
+            MatrixF = new List<List<int>>(){
+                new List<int>(){0,1,2,3,4,5},
+                new List<int>(){5,0,1,2,3,4},
+                new List<int>(){4,5,0,1,2,3},
+                new List<int>(){3,4,5,0,1,2},
+                new List<int>(){2,3,4,5,0,1},
+                new List<int>(){1,2,3,4,5,0}
+            };*/
+                MatrixF = new List<List<int>>(){
+                new List<int>(){0, 1, 2 ,3, 1, 2, 3 ,4 ,2 ,3 ,4 ,5},
+                new List<int>(){1, 0, 1, 2, 2, 1, 2, 3, 3, 2 ,3 ,4},
+                new List<int>(){2, 1, 0, 1, 3, 2, 1, 2 ,4 ,3 ,2 ,3},
+                new List<int>(){3, 2, 1, 0 ,4 ,3 ,2 ,1 ,5 ,4, 3, 2},
+                new List<int>(){1, 2, 3, 4 ,0 ,1 ,2 ,3 ,1 ,2 ,3 ,4 },
+                new List<int>(){2, 1, 2, 3, 1 ,0 ,1 ,2 ,2 ,1 ,2 ,3, },
+                new List<int>(){3, 2 ,1 ,2 ,2 ,1 ,0 ,1 ,3 ,2, 1 ,2 },
+                new List<int>(){4, 3, 2, 1, 3, 2 ,1 ,0 ,4, 3, 2, 1 },
+                new List<int>(){2, 3, 4 ,5 ,1 ,2 ,3 ,4 ,0 ,1 ,2 ,3 },
+                new List<int>(){3, 2 ,3 ,4 ,2 ,1 ,2 ,3 ,1 ,0, 1, 2 },
+                new List<int>(){4, 3, 2, 3 ,3 ,2 ,1 ,2 ,2 ,1 ,0 ,1 },
+                new List<int>(){5, 4, 3, 2, 4 ,3 ,2 ,1 ,3 ,2 ,1 ,0 }
+            };
+
+            MatrixD = new List<List<int>>(){
+                new List<int>(){0 , 5 , 2 , 4,  1,  0,  0,  6,  2,  1,  1,  1},
+                new List<int>(){5,  0,  3,  0,  2,  2,  2,  0,  4,  5,  0,  0},
+                new List<int>(){2,  3,  0,  0,  0,  0,  0,  5,  5,  2,  2,  2},
+                new List<int>(){ 4,  0,  0,  0,  5,  2,  2, 10,  0,  0,  5,  5 },
+                new List<int>(){ 1,  2,  0,  5,  0, 10,  0,  0,  0,  5,  1,  1},
+                new List<int>(){0,  2,  0,  2, 10,  0,  5,  1,  1,  5,  4,  0 },
+                new List<int>(){0,  2,  0,  2,  0,  5,  0, 10,  5,  2,  3,  3 },
+                new List<int>(){6,  0,  5, 10,  0,  1, 10,  0,  0,  0,  5,  0},
+                new List<int>(){ 2,  4,  5,  0,  0,  1,  5,  0,  0,  0, 10, 10},
+                new List<int>(){ 1,  5,  2,  0,  5,  5,  2,  0,  0,  0,  5,  0},
+                new List<int>(){1,  0,  2,  5,  1,  4,  3,  5, 10,  5,  0,  2},
+                new List<int>(){1,  0,  2,  5,  1,  0,  3,  0, 10,  0,  2,  0 }
+            };
+        }
+
+        public InputData(List<List<int>> MatrixD_, List<List<int>> MatrixF_)
+        {
+            MatrixD = MatrixD_;
+            MatrixF = MatrixF_;
+        }
+
+        public int ObjectiveFunction(List<int> NewLayout )
+        {
+            int Value = 0;
+            for (int i = 0; i<NewLayout.Count; i++)
+            {
+                for(int j = 0; j<NewLayout.Count; j++)
+                {
+                    if (j != i)
+                    {
+                        Value += MatrixD[NewLayout[i] - 1][NewLayout[j] - 1] * MatrixF[i][j];
+                    }
+                }
+            }
+            return Value;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Net;
+
+namespace QAP
+{
+    class Program
+    {
+        public static int GiveRandomNumber(List<int> list, int max_value, Random rand)
+        {
+            var range = Enumerable.Range(1, max_value).Where(i => !list.Contains(i));
+            int index = rand.Next(0, max_value - list.Count);
+            return range.ElementAt(index);
+        }
+
+        public static List<List<int>> GenerateFirstPopulation(int size)
+        {
+            var rand = new System.Random();
+            List<List<int>> FirstPopulation = new List<List<int>>();
+
+            for (int i = 0; i < size; i++)
+            {
+                List<int> Individual = new List<int>();
+                for (int j = 0; j < size; j++)
+                {
+                    Individual.Add(GiveRandomNumber(Individual, size, rand));
+                }
+                FirstPopulation.Add(Individual);
+            }
+
+            return FirstPopulation;
+        }
+
+        public static void Normalize(List<double> MutatedIndividual_tmp , List<int> MutatedIndividual)
+        {
+            List<double> ListToConvert = MutatedIndividual_tmp;
+            int cnt = 1;
+
+            while(cnt != (MutatedIndividual_tmp.Count+1))
+            {
+                 var index = ListToConvert.FindIndex(min => min==MutatedIndividual_tmp.Min());
+                MutatedIndividual[index] = cnt;
+                MutatedIndividual_tmp[index] = Double.MaxValue;//.RemoveAt(MutatedIndividual_tmp.FindIndex(min => min == MutatedIndividual_tmp.Min()));
+
+                cnt++;
+            }
+
+        }
+
+        public static List<int> Mutation(List<List<int>> Population, int i)
+        {
+            List<int> MutatedIndividual = new List<int>(new int[Population.Count]);
+            List<double> MutatedIndividual_tmp = new List<double>();
+            List<int> WhichIndividuals = new List<int>() {i};
+            var rand = new System.Random();
+            int number = 3;
+            double DefaultValueForMut = 0.8;
+
+            while(number != 0)
+            {
+                WhichIndividuals.Add(GiveRandomNumber(WhichIndividuals, Population[i].Count-1, rand));
+                number--;
+            }
+
+            for (int j = 0; j<Population[i].Count; j++)
+            {
+                double value = Population[WhichIndividuals[1]][j] + DefaultValueForMut * (Population[WhichIndividuals[2]][j] - Population[WhichIndividuals[3]][j]);
+                MutatedIndividual_tmp.Add(value);
+            }
+
+            Normalize(MutatedIndividual_tmp, MutatedIndividual);
+
+            return MutatedIndividual;
+        }
+
+        public static List<int> BinomialCrossver(List<int> ParentIndividual, List<int> MutatedIndividual)
+        {
+            List<int> CrossedIndividual = new List<int>();
+            Random random = new Random();
+            double Cr = random.NextDouble();
+
+            Random random2 = new Random();
+
+            int size = ParentIndividual.Count;
+            for (int i = 0; i< size; i++)
+            {
+                double Cr_tmp = random.NextDouble();
+
+                if (Cr_tmp < Cr) // Mutated
+                {
+                    if (CrossedIndividual.Contains(MutatedIndividual[i]))
+                    {
+                        var ind = GiveRandomNumber(CrossedIndividual, MutatedIndividual.Count, random2);
+                        CrossedIndividual.Add(ind);
+                    }
+                    else
+                        CrossedIndividual.Add(MutatedIndividual[i]);
+                }
+                else //Parent
+                {
+                    if (CrossedIndividual.Contains(ParentIndividual[i]))
+                    {
+                        var ind = GiveRandomNumber(CrossedIndividual, ParentIndividual.Count, random2);
+                        CrossedIndividual.Add(ind);
+                    }
+                    else
+                        CrossedIndividual.Add(ParentIndividual[i]);
+                }
+            }
+            return CrossedIndividual;
+        }
+
+        public static void Replacement(List<List<int>> FirstPopulation, List<int> CrossedIndividual, int i)
+        {
+            InputData inputdata = new InputData();
+
+            if (inputdata.ObjectiveFunction(CrossedIndividual) < inputdata.ObjectiveFunction(FirstPopulation[i])){ 
+                FirstPopulation.RemoveAt(i);
+                FirstPopulation.Insert(i, CrossedIndividual);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Dane wejściowe
+            InputData inputdata = new InputData();
+
+            // Generacja pierwszej populacji 
+            List<List<int>> FirstPopulation = GenerateFirstPopulation(inputdata.MatrixD.Count);
+
+            int num_of_iter = 100;
+
+            while (num_of_iter != 0)
+            {
+                for (int i = 0; i < inputdata.MatrixD.Count; i++)
+                {
+                    // Mutacja
+                    List<int> MutatedIndividual = Mutation(FirstPopulation, 0);
+
+                    // Krzyżowanie
+                    List<int> CrossedIndividual = BinomialCrossver(FirstPopulation[0], MutatedIndividual);
+
+                    // Replacement
+                    Replacement(FirstPopulation, CrossedIndividual, i);
+
+                    Console.WriteLine("------------------------");
+                    foreach (var vec in FirstPopulation)
+                    {
+                        Console.Write(inputdata.ObjectiveFunction(vec) + " ");
+                        foreach (var ob in vec)
+                        {
+                            Console.Write(ob + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                Console.WriteLine("------------------------");
+                num_of_iter--;
+            }
+
+
+
+
+
+            /*foreach(var i in MutatedIndividual)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /* foreach (var dat in FirstPopulation)
+             {
+                 dat.ForEach(Console.Write);
+                 Console.WriteLine('\n');
+             }*/
+            Console.ReadKey();
+        }
+    }
+}
